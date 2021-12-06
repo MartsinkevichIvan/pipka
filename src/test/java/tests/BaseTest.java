5ff3d00b2f8da6997501b05d56349ab1d1dbf7ea
Aliaksandr Dyrykau <aliaksandr_dyrykau@epam.com>
2021-12-06 14:10:14 +0300

    add listeners to BaseTest.java

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,11 +1,15 @@
 package tests;
 
+import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import driver.SingletonDriver;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Listeners;
 import spring.SpringConfig;
+import utils.TestListener;
 
+@Listeners({TestListener.class, ReportPortalTestNGListener.class})
 @ContextConfiguration(classes = SpringConfig.class)
 public class BaseTest extends AbstractTestNGSpringContextTests{
 
