f64b9f7afbb91f551b58cd9ef4475159ea01c60e
Ivan_Martsinkevich <Ivan_Martsinkevich@epam.com>
2022-01-14 11:23:41 +0300

    adsa

--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -9,7 +9,7 @@ import pages.adminPages.SettingsPage;
 
 import static utils.Wait.*;
 
-@Test (groups = "web")
+@Test (groups = "web1")
 public class LoginPageTest extends BaseTest {
     @Autowired
     LoginPage loginPage;
--- src/test/java/tests/SingleTest.java
+++ src/test/java/tests/SingleTest.java
@@ -3,7 +3,7 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-@Test(groups = "web1")
+@Test(groups = "web")
 public class SingleTest extends BaseTest{
 
     @Test
