0e3255116829c6184fb76a3c117a7ae267eb72c6
Ivan_Martsinkevich <Ivan_Martsinkevich@epam.com>
2022-01-17 01:10:28 +0300

    sfasd

--- src/test/java/tests/appiumTests/BaseAppiumTest.java
+++ src/test/java/tests/appiumTests/BaseAppiumTest.java
@@ -2,16 +2,18 @@ package tests.appiumTests;
 
 import appiumDriver.DriverManager;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 public class BaseAppiumTest {
 
-    @BeforeTest
+    @BeforeMethod(groups = "mobile")
     public void createSession() {
         DriverManager.getAppiumDriver().get("https://www.youtube.com");
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterTest(alwaysRun = true)
     public void closeSession() {
         DriverManager.closeAppiumDriver();
         DriverManager.closeAppium();
